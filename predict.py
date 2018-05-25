4a378fdeb5ad130191246122809bfa1444a315a4
mistery <mistery.zhang@gmail.com>
2018-05-25 21:37:22 +0800

    add file: predict.py

---
+++ predict.py
@@ -0,0 +1,11 @@
+# !usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2018-05-25
+@author: mistery
+@version: 1.0.0
+
+reference:
+[1]https://www.cnblogs.com/lijunjiang2015/p/7812996.html
+'''
+import vcr

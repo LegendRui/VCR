46f834f51ac3db208ef9f0e6a109fcf597421e88
mistery <mistery.zhang@gmail.com>
2018-05-25 22:40:10 +0800

    modify file: predict.py

--- predict.py
+++ predict.py
@@ -7,5 +7,22 @@ Created on 2018-05-25
 
 reference:
 [1]https://www.cnblogs.com/lijunjiang2015/p/7812996.html
+[2]https://blog.csdn.net/huachao1001/article/details/78501928
 '''
-import vcr
+
+import generate_train_imgs as gti
+import cv2
+
+test_dir = './test/0A20.bmp'
+img = cv2.imread(test_dir, cv2.IMREAD_GRAYSCALE)
+cv2.imshow('0A20.bmp', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+bin_img = gti.get_bin_img(img)
+sub_imgs = gti.img_split(bin_img)
+for sub_img in sub_imgs:
+    cv2.imshow('sub_img', sub_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
